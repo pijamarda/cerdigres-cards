bea7e470636ee7e862436c41af5293f9c36160c6
Fernando Gimenez <zeneke@gmail.com>
2018-05-17 17:17:06 +0200

    new models and files

--- ccSite/ccSite/settings.py
+++ ccSite/ccSite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rank.apps.RankConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- ccSite/rank/models.py
+++ ccSite/rank/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Card(models.Model):
+    card_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.card_text
+
+class Rulebook(models.Model):
+    rulebook_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.rulebook_text
