458dbfeb8aec470895654aaf0be61826113de18d
fgimenez <fgimenez@NZXT-I7>
2018-05-18 23:46:12 +0200

    registration of users working

--- ccSite/home/urls.py
+++ ccSite/home/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from django.urls import include, path
 
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
 from . import views
 urlpatterns = [
+    path('register/', CreateView.as_view(
+            template_name='registration/register.html',
+            form_class=UserCreationForm,
+            success_url='/'
+    )),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', views.index, name='index'),
 ]
